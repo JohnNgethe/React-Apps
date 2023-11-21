ba93aabc88c7703f85893979ed2f94c7c3048aa6
JohnNgethe <Jngethe07@gmail.com>
2023-11-22 00:26:26 +0300

    Added .map function to render several notes

--- keeper-app-part-1-starting/src/App.jsx
+++ keeper-app-part-1-starting/src/App.jsx
@@ -2,12 +2,19 @@ import React from "react";
 import Header from "./components/Header";
 import Note from "./components/Note";
 import Footer from "./components/Footer";
+import notes from "./notes";
 
 function App(params) {
   return (
     <div>
       <Header />
-      <Note/>
+      {notes.map((noteItem) => (
+        <Note
+          key={noteItem.key}
+          title={noteItem.title}
+          content={noteItem.content}
+        />
+      ))}
       <Footer />
     </div>
   );
